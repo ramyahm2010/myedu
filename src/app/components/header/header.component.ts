45d0e1d6b7776aa890ad17699a4a43391e36cd48
Ramy Ahmed <ramy.ahm2010@gmail.com>
2024-12-12 14:31:11 +0200

    Create shared (app-header) component

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { IonToolbar, IonTitle, IonHeader } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  standalone: true,
+  imports: [IonHeader, IonToolbar, IonTitle],
+})
+export class HeaderComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonContent, IonGrid, IonRow, IonCol } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonContent, IonGrid, IonRow, IonCol],
 })
 export class HomePage {
   constructor() {}
