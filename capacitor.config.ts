22435d1ce22563320d867197a1d2013996748473
Ramy Ahmed <ramy.ahm2010@gmail.com>
2024-12-12 10:01:35 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'myedu-app',
+  webDir: 'www'
+};
+
+export default config;
